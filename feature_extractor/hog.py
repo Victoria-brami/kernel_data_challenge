9b51f482b6ad92a1df13e9ba78c22745a7c08ddf
unknown <victoria.brami88@gmail.com>
2022-03-08 11:28:35 +0100

    corrected implementation (works for now)

--- feature_extractor/hog.py
+++ feature_extractor/hog.py
@@ -1,5 +1,6 @@
 import numpy as np
 from load import Data
+import sys
 
 
 class FeatureExtractor:
@@ -10,118 +11,210 @@ class FeatureExtractor:
 
 class HOG(FeatureExtractor):
     """ Computes the historgam of Oriented Gradients"""
-    def __init__(self, cell_size=4):
+    def __init__(self, pixels_per_cell=4, cells_per_block=3, orientations=9):
         super(HOG, self).__init__()
-        self.cell_size = cell_size
+        self.cell_size = pixels_per_cell
         self.H = 32
         self.W = 32
         self.nb_cell_cols = int(self.H // self.cell_size)
         self.nb_cell_rows = int(self.W // self.cell_size)
-        self.nb_bins = 9
+        self.nb_bins = orientations
+        self.cells_per_block = cells_per_block
         self.features = []
 
-
-    def compute_gradients_per_channel(self, image, n_channel=0):
-        Jx = np.zeros((self.H, self.W))
-        Jy = np.zeros((self.H, self.W))
-        for i in range(1, self.H - 1):
-            for j in range(1, self.W - 1):
-                Jy[i, j] = (image[n_channel, i + 1, j] - image[n_channel, i - 1, j]) / 2
-                Jx[i, j] = (image[n_channel, i, j + 1] - image[n_channel, i, j - 1]) / 2
-
-        for i in range(self.H):
-            Jx[i, 0] = image[n_channel, i, 1] - image[n_channel, i, 0]
-            Jx[i, -1] = image[n_channel, i, -1] - image[n_channel, i, -2]
-
-        for j in range(self.W):
-            Jy[0, j] = image[n_channel, 1, j] - image[n_channel, 0, j]
-            Jy[-1, j] = image[n_channel, -1, j] - image[n_channel, -2, j]
-
-        return Jx, Jy
-
     def compute_gradients(self, image):
-        gradients_x = np.zeros((3, self.H, self.W))
-        gradients_y = np.zeros((3, self.H, self.W))
-        for channel in range(3):
-            gradients_x[channel], gradients_y[channel]= self.compute_gradients_per_channel(image, channel)
-        return gradients_x, gradients_y
+        g_x = np.zeros((3, self.H, self.W))
+        g_y = np.zeros((3, self.H, self.W))
+        # On the edges of the image
+        g_x[:, :, 0] = 0
+        g_x[:, :, -1] = 0
+        g_y[:, 0, :] = 0
+        g_y[:, -1, :] = 0
+        # Inside the image
+        g_x[:, :, 1:-1] = image[:, :, 2:] - image[:, :, :-2]
+        g_y[:, 1:-1, :] = image[:, 2:, :] - image[:, :-2, :]
+        return g_x, g_y
 
-    def compute_direction(self, gradients_x, gradients_y):
-        theta = np.zeros(gradients_x.shape)
-        theta = np.arctan(gradients_y / gradients_x)
-        return theta
+    def compute_grey_gradients(self, image):
+        g_x = np.zeros((self.H, self.W))
+        g_y = np.zeros((self.H, self.W))
+        # On the edges of the image
+        g_x[:, 0] = 0
+        g_x[:, -1] = 0
+        g_y[0, :] = 0
+        g_y[-1, :] = 0
+        # Inside the image
+        g_x[:, 1:-1] = image[:, 2:] - image[:, :-2]
+        g_y[1:-1, :] = image[2:, :] - image[:-2, :]
+        return g_x, g_y
 
-    def compute_magnitude(self, gradients_x, gradients_y):
-        magnitude = np.sqrt(gradients_x**2 + gradients_y**2)
+
+    def compute_direction(self, gradients_x, gradients_y, eps=1e-6):
+        theta = np.arctan2(gradients_y , gradients_x)
+        grad_direction = np.rad2deg(theta)
+        grad_direction = grad_direction % 180
+        return grad_direction
+
+    def compute_magnitude(self, g_x, g_y):
+        magnitude = np.sqrt(g_x**2 + g_y**2)
         return magnitude
 
-    def compute_cell_hog_feature(self, cell_image, cell_magnitudes, cell_directions):
-        hist_bins = np.arange(0, 180, 20)
-        histogram = np.zeros(self.nb_bins)
-        cell_H, cell_W = cell_image.shape[0], cell_image.shape[1]
-        for row_idx in range(cell_H):
-            for col_idx in range(cell_W):
-                magnitude_value = cell_magnitudes[row_idx, col_idx]
-                direction_value = cell_directions[row_idx, col_idx] * 180 / np.pi
 
-                # Get the two closest bins depending on the direction value
-                list_ids = np.where(direction_value * np.ones(self.nb_bins) - hist_bins < 0)[0]
-                print("Direction value", direction_value)
+    def _compute_cell_hog(self, cell_magnitude, cell_direction, direction_inf, direction_sup):
+        """" A CORRIGER """
+        tot_hog = 0.
+        for channel in range(3):
+            for cell_i in range(self.cell_size):
+                for cell_j in range(self.cell_size):
+                    if (cell_direction[channel, cell_i, cell_j] >= direction_inf) or (cell_direction[channel, cell_i, cell_j] < direction_sup):
+                        continue
+                    tot_hog += cell_magnitude[channel, cell_i, cell_j]
+        return tot_hog / (self.cell_size * self.cell_size)
 
-                if hist_bins[-1] < direction_value or direction_value < hist_bins[0]:
-                    closest_ids = (0, len(hist_bins) -1)
-                    if abs(direction_value - hist_bins[closest_ids[0]]) < abs(direction_value - hist_bins[closest_ids[1]]):
-                        closest_id = closest_ids[0]
-                        second_closest_id = closest_ids[1]
-                    else:
-                        closest_id = closest_ids[1]
-                        second_closest_id = closest_ids[0]
-                else:
-                    print("List ids", list_ids)
-                    closest_ids = (list_ids[0]-1, list_ids[0])
-                    if direction_value - hist_bins[closest_ids[0]] < direction_value - hist_bins[closest_ids[1]]:
-                        closest_id = closest_ids[0]
-                        second_closest_id = closest_ids[1]
-                    else:
-                        closest_id = closest_ids[1]
-                        second_closest_id = closest_ids[0]
-
-                ratio = abs(direction_value - hist_bins[closest_id]) / (hist_bins[1] - hist_bins[0])
-                histogram[closest_id] += ratio * magnitude_value
-                histogram[second_closest_id] += (1 - ratio) * magnitude_value
-
-        return histogram
 
     def compute_hog_features(self, image):
-        gradients_x, gradients_y = self.compute_gradients(image)
-        magnitude = self.compute_magnitude(gradients_x, gradients_y)
-        direction = self.compute_direction(gradients_x, gradients_y)
 
+        l_0 = self.cell_size / 2
+        c_0 = self.cell_size / 2
+
+        g_x, g_y = self.compute_gradients(image)
+        magnitude = self.compute_magnitude(g_x, g_y)
+        direction = self.compute_direction(g_x, g_y)
         hist = np.zeros((3, self.nb_cell_cols, self.nb_cell_rows, self.nb_bins))
 
+        range_rows_stop = (self.cell_size + 1) / 2
+        range_rows_start = -(self.cell_size / 2)
+        range_columns_stop = (self.cell_size + 1) / 2
+        range_columns_start = -(self.cell_size / 2)
+
         for channel in range(3):
-            for i in range(self.nb_cell_rows):
-                for j in range(self.nb_cell_cols):
-                    cell_image = image[channel, i*self.cell_size:(i+1)*self.cell_size, j*self.cell_size:(j+1)*self.cell_size]
-                    cell_magnitude = magnitude[channel, i*self.cell_size:(i+1)*self.cell_size, j*self.cell_size:(j+1)*self.cell_size]
-                    cell_direction = direction[channel, i*self.cell_size:(i+1)*self.cell_size, j*self.cell_size:(j+1)*self.cell_size]
-                    hist[channel, i, j, :] = self.compute_cell_hog_feature(cell_image, cell_magnitude, cell_direction)
+            for i in range(self.nb_bins):
+                # Bounds of orientations
+                start_direction = (i+1) * 180./self.nb_bins
+                end_direction = i * 180./self.nb_bins
+                l = l_0
+                c = c_0
+                l_i = 0
+                c_i = 0
+
+                while l < self.H:
+                    c_i = 0
+                    c = c_0
+
+                    while c < self.W:
+                        cell_magnitude = magnitude[channel, l+range_rows_start:l+range_rows_stop, c+range_columns_start:c+range_columns_stop]
+                        cell_direction =  direction[channel, l+range_rows_start:l+range_rows_stop, c+range_columns_start:c+range_columns_stop]
+                        hist[channel, l_i, c_i, i] = self._compute_cell_hog(self, cell_magnitude, cell_direction, start_direction, end_direction)
+
+                        c_i += 1
+                        c += self.cell_size
+                    l_i += 1
+                    l += self.cell_size
+
         hog_features = np.reshape(hist, -1)
         return hog_features
+
 
     def _compute_features(self, X):
         NB_IMAGES = X.shape[0]
         for i in range(NB_IMAGES):
+            sys.stdout.write("\r Computing feature .... [{} / {}]".format(i, NB_IMAGES))
             image = X[i]
-            print("IMAGE SHAPE", image.shape)
             self.features.append(self.compute_hog_features(image))
+        return np.array(self.features)
+
+
+    def _compute_cell_grey_hog(self, cell_magnitude, cell_direction, direction_inf, direction_sup):
+        """" A CORRIGER """
+        tot_hog = 0.
+        for cell_i in range(self.cell_size):
+            for cell_j in range(self.cell_size):
+                if (cell_direction[cell_i, cell_j] >= direction_inf) or (cell_direction[cell_i, cell_j] < direction_sup):
+                    continue
+                tot_hog += cell_magnitude[cell_i, cell_j]
+        return tot_hog / (self.cell_size * self.cell_size)
+
+
+    def compute_hog_grey_features(self, image):
+
+        l_0 = self.cell_size / 2
+        c_0 = self.cell_size / 2
+
+        g_x, g_y = self.compute_grey_gradients(image)
+        magnitude = self.compute_magnitude(g_x, g_y)
+        direction = self.compute_direction(g_x, g_y)
+        hist = np.zeros((self.nb_cell_cols, self.nb_cell_rows, self.nb_bins))
+
+        range_rows_stop = (self.cell_size + 1) / 2
+        range_rows_start = -(self.cell_size / 2)
+        range_columns_stop = (self.cell_size + 1) / 2
+        range_columns_start = -(self.cell_size / 2)
+
+        for i in range(self.nb_bins):
+            # Bounds of orientations
+            start_direction = (i+1) * 180./self.nb_bins
+            end_direction = i * 180./self.nb_bins
+            l = l_0
+            c = c_0
+            l_i = 0
+            c_i = 0
+
+            while l < self.H:
+                c_i = 0
+                c = c_0
+
+                while c < self.W:
+                    range_rows_start = int(range_rows_start)
+                    range_rows_stop = int(range_rows_stop)
+                    range_columns_start = int(range_columns_start)
+                    range_columns_stop = int(range_columns_stop)
+                    cell_magnitude = magnitude[int(l+range_rows_start):int(l+range_rows_stop), int(c+range_columns_start):int(c+range_columns_stop)]
+                    cell_direction =  direction[int(l+range_rows_start):int(l+range_rows_stop), int(c+range_columns_start):int(c+range_columns_stop)]
+                    hist[l_i, c_i, i] = self._compute_cell_grey_hog(cell_magnitude, cell_direction, start_direction, end_direction)
+
+                    c_i += 1
+                    c += self.cell_size
+                l_i += 1
+                l += self.cell_size
+        return hist
+
+    """ TO MODIFY (TAKEN FROM SKIMAGE) """
+    def _normalize_descriptors(self, image_histogram):
+
+        n_blocks_row = (self.H // self.cell_size - self.cells_per_block) + 1
+        n_blocks_col = (self.W // self.cell_size - self.cells_per_block) + 1
+        normalized_blocks = np.zeros(
+            (n_blocks_row, n_blocks_col, self.cells_per_block,self.cells_per_block, self.nb_bins)
+        )
+        for r in range(n_blocks_row):
+            for c in range(n_blocks_col):
+                block = image_histogram[r:r + self.cells_per_block, c:c + self.cells_per_block, :]
+                normalized_blocks[r, c, :] = self._normalize_block(block)
+
+        return normalized_blocks.reshape(-1)
+
+    def _compute_grey_features(self, X):
+        NB_IMAGES = X.shape[0]
+        for i in range(NB_IMAGES):
+            sys.stdout.write("\r Computing feature .... [{} / {}]".format(i, NB_IMAGES))
+            image = X[i]
+            hog_feature = self.compute_hog_grey_features(image)
+            normalized_hog_feature = self._normalize_descriptors(hog_feature)
+            self.features.append(normalized_hog_feature)
+        return np.array(self.features)
+
+    def _normalize_block(self, block, eps=1e-6):
+        res = block / np.sqrt(np.sum(block ** 2) + eps ** 2)
+        res = np.minimum(res, 0.2)
+        res = res / np.sqrt(np.sum(res ** 2) + eps ** 2)
+        return res
+
+
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
 
-
-    hog = HOG()
     data = Data()
-    Xte = data.Xte_im
-    feats = hog._compute_features(Xte)
-    print(feats.shape)
+    Xte = data.grey_Xte_im
+    Xtr = data.grey_Xtr_im
+
+    hogb = HOG(pixels_per_cell=8)
