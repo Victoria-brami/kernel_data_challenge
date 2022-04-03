70233d69b2285728f9274d2872e1fc1c3762da0a
Margot Cosson <margotcosson@macbook-air-de-margot.home>
2022-04-03 11:58:47 +0200

    fix bug accuracy

--- predict.py
+++ predict.py
@@ -55,7 +55,7 @@ def predict(args, dev0=None):
     # Train the classifier
     classifier.fit(train_features, Ytr.reshape(-1))
     Y_train_preds, pred_probas = classifier.predict(train_features)
-    get_accuracy(Y_train_preds, Ytr, verbose=True)
+    get_accuracy(Y_train_preds.get(), Ytr.get(), verbose=True)
     Ytrain = {'Prediction': Y_train_preds.get()}
     dataframe = pd.DataFrame(Ytrain)
     dataframe.index += 1
